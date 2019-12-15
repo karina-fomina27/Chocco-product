462360435d0f98028d03a5fb01f0ac5d6cb2af05
Karina Fomina <karina_fomina27@mail.ru>
2019-12-15 21:36:42 +0300

    Реализация формы заказа

--- js/form.js
+++ js/form.js
@@ -1,30 +1,38 @@
+
 const myForm = document.querySelector('#myForm');
 const sendButton = document.querySelector('#sendButton');
 
-sendButton.addEventListener('click', function(event){
-event.preventDefault();
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-if(validateForm(myForm)){
+  const formData = new FormData();
+  formData.append('name', myForm.elements.name.value);
+  formData.append('phone', myForm.elements.phone.value);
+  formData.append('comment', myForm.elements.comment.value);
+  formData.append('to', 'form@gmail.com');
 
-}
-
+   var xhr = new XMLHttpRequest();
+   xhr.responseType = 'json';
+   xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+   xhr.send(formData);
 });
 
 function validateForm(form) {
   let valid = true;
 
-  if(!validateField(form.elements.name)) {
+  if (!validateField(form.elements.name)) {
     valid = false;
   }
-  if(!validateField(form.elements.phone)) {
+  if (!validateField(form.elements.phone)) {
     valid = false;
   }
 
   return valid;
 }
 
+function validateField(field) {
+  field.nextElementSiblink.textContent = field.validationMassage;
 
+  return field.checkValidity();
 
-
-
-
+}
--- js/mine.js
+++ js/mine.js
@@ -13,7 +13,7 @@ $(function () {
 
       parent.removeClass(active);
 
-    } else{
+    } else {
 
       btn.parent().removeClass(active);
       parent.addClass(active);
@@ -29,34 +29,16 @@ $(function () {
 // });
 
 
-// Слайд-шоу
-  (function() {
-  let carouselItems = document.querySelectorAll('.carousel__item'),
-    carouselItemsLength = carouselItems.length,
-  reviewsBlockItems = document.querySelectorAll('.reviews-list__item');
-
-  for (let i = 0; i < carouselItemsLength; ++i) {
-    carouselItems[i].addEventListener('click', function (e) {
-      for (let j = 0; j < carouselItemsLength; ++j) {
-        carouselItems[j].classList.remove('carousel__item--active');
-        reviewsBlockItems[j].classList.remove('reviews-list__item--active');
-      };
-      e.currentTarget.classList.add('carousel__item--active');
-      reviewsBlockItems[i].classList.add('reviews-list__item--active');
-    });
-  }
-})()
-
 
 // Вертикальный аккордион
 const trianglesList = Array.from(document.querySelectorAll('.worker__name-pic'));
 
-for(let triangle of trianglesList){
+for (let triangle of trianglesList) {
 
-triangle.addEventListener('click', function(e){
-  e.target.classList.toggle('worker__name-pic--rotated');
-  const elemToShow = e.target.parentElement.parentElement.querySelector ('.worker__descrip');
-  elemToShow.classList.toggle('worker__descrip--expanted');
+  triangle.addEventListener('click', function (e) {
+    e.target.classList.toggle('worker__name-pic--rotated');
+    const elemToShow = e.target.parentElement.parentElement.querySelector('.worker__descrip');
+    elemToShow.classList.toggle('worker__descrip--expanted');
 
   });
 
@@ -67,14 +49,14 @@ const mymodal = document.querySelector("#mymodal");
 const burgerOpen = document.querySelector("#burger-wrap");
 const close = document.querySelector(".burger_active");
 
-burgerOpen .addEventListener("click", function () {
+burgerOpen.addEventListener("click", function () {
   mymodal.classList.toggle("modal--active");
-    document.body.classList.toggle("body--active ");
+  document.body.classList.toggle("body--active ");
 });
 
 close.addEventListener("click", function () {
   mymodal.classList.toggle("modal--active");
-    document.body.classList.toggle("body--active");
-    console.log('click');
+  document.body.classList.toggle("body--active");
+  console.log('click');
 });
 
